f1f4eb1278b61d2bf6795473fcaa34257f46d66c
LiuNightmare <liujiabo810@gmail.com>
2023-12-24 17:32:41 +0800

    修改了sql

--- order_insert.sql
+++ order_insert.sql
@@ -1,10 +1,7 @@
-DROP TABLE IF EXISTS `chart_user`;
-CREATE TABLE `chart_user` (
-  `uid` tinyint(4) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-INSERT INTO `chart_user` VALUES ('1', 'aa', '1');
-INSERT INTO `chart_user` VALUES ('2', 'bb', '1');
-INSERT INTO `chart_user` VALUES ('3', 'cc', '1');
+INSERT INTO `product` (`pid`, `product_name`, `description`, `picture`, `quantity`, `price`) VALUES ('1', 'omni-Orxnge', '8V714kK2KS', 'computer.jpg', 564, 123);
+
+INSERT INTO `link` (`lid`, `pid`, `sell_uid`) VALUES ('1', '1', 'visitor1');
+
+INSERT INTO `order` (`oid`, `lid`, `buy_uid`, `Oquantity`, `total`, `Otime`, `Status`, `Lnumber`) VALUES ('1', '1', 'admin1', 605, 5, '09:41:07', 'unreceived', 'QRPpbzbXra');
+INSERT INTO `order` (`oid`, `lid`, `buy_uid`, `Oquantity`, `total`, `Otime`, `Status`, `Lnumber`) VALUES ('2', '1', 'admin1', 605, 5, '09:41:07', 'unsent', 'QRPpbzbXra');
+INSERT INTO `order` (`oid`, `lid`, `buy_uid`, `Oquantity`, `total`, `Otime`, `Status`, `Lnumber`) VALUES ('3', '1', 'admin1', 605, 5, '09:41:07', 'received', 'QRPpbzbXra');
