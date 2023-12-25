c775226fda174ee291a6726cca1b11ad2c161965
taogeshuaibaole <1006007027@qq.com>
2023-12-25 20:13:04 +0800

    merge

--- src/main/java/boot/spring/service/ProductService.java
+++ src/main/java/boot/spring/service/ProductService.java
@@ -1,12 +1,17 @@
 package boot.spring.service;
 
+import boot.spring.po.Order;
 import boot.spring.po.Product;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Component
 public interface ProductService {
     //存商品信息
     void saveProduct(Product product);
+    List<Product> getHistoryByUid(String Uid);
+
 }
