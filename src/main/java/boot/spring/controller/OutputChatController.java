e9bd661b1acf098bed739f26fa2094358fec3ae9
kalok Yau <yaukalokevin@gmail.com>
2023-12-26 09:59:52 +0800

    聊天记录 移位

--- src/main/java/boot/spring/controller/OutputChatController.java
+++ src/main/java/boot/spring/controller/OutputChatController.java
@@ -1,15 +1,20 @@
 package boot.spring.controller;
 
 import boot.spring.po.OutputChat;
+import boot.spring.po.User;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
 import boot.spring.service.OutputChatService;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -32,4 +37,10 @@ public class OutputChatController {
         m.addAttribute("userAll", outputChats);
         return "/OutputChat";
     }
+    // @RequestMapping(value = "/currentuser", method = RequestMethod.GET)
+	// @ResponseBody
+	// public User currentuser(HttpSession httpSession) {
+	// 	User user = (User) httpSession.getAttribute("user");
+	// 	return user;
+	//}
 }
